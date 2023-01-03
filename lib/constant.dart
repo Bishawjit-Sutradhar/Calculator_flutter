4cfb4bffc8d0a49768a165c8cc3187f48e69fd34
Bishawjit Sutradhar <bishawjit650@gmail.com>
2023-01-03 14:58:44 +0600

    final version

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+
+const Color grayColor=Colors.grey;
+const Color orangeColor=Colors.orange;
+const Color whiteColor=Colors.white;
+
+const headingTextStyle=TextStyle(fontSize: 15,color: Colors.white,fontWeight: FontWeight.w500);
